29e32e5db0f41a8a06fcac07f795616164a91135
Robert Kowalski <rok@lavabit.com>
2011-12-31 21:51:23 +0100

    initial commit to github

---
+++ README.md
@@ -0,0 +1,55 @@
+Phonegap version of the CCCHH App
+=================================
+
+Tests
+-----
+
+```
+rake jasmine
+
+```
+
+iOS Phonegap Settings
+---------------------
+
+PhoneGap.plist
+
+set ExternalHosts to `*.*`
+
+
+Open Source Projects used
+-------------------------
+
+[Jasmine](https://github.com/pivotal/jasmine)
+
+[Jasmine jQuery](https://github.com/velesin/jasmine-jquery)
+
+[Jasmine Ajax](https://github.com/pivotal/jasmine-ajax)
+
+[jQuery](https://github.com/jquery/jquery)
+
+[jQuery Mobile](https://github.com/jquery/jquery-mobile)
+
+[hogan.js](https://github.com/twitter/hogan.js)
+
+[Phonegap](https://github.com/phonegap)
+
+Icons
+-----
+
+Interactivemania [(Creative Commons Attribution-No Derivative Works 3.0 Unported)](http://creativecommons.org/licenses/by-nd/3.0/)
+
+License
+-------
+
+Copyright (c) 2012, R. Kowalski
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
---
+++ www/javascripts/App.js
@@ -0,0 +1,168 @@
+App = {
+
+  dhcpUrl : 'http://bananentage.de/rainerfoobarbatnarfkrams.txt',
+
+  doorUrl: 'http://dooris.koalo.de/door.txt',
+
+  doorTemplate: '',
+  dhcpTemplate: '',
+
+  init: function() {
+    var self = this,
+        dhcpHtml = '<td><img src="images/computer.png" /></td><td class="bottom"><div id="clientnumber" class="font">{{clientCount}}</div></td>',
+        doorHtml = '<td><img src="images/{{openLocked}}"/></td><td class="bottom"><div class="font">{{time}}</div></td>';
+
+    self.dhcpTemplate = Hogan.compile(dhcpHtml);
+    self.doorTemplate = Hogan.compile(doorHtml);
+
+    self.bindToView();
+    self.tick();
+
+  },
+
+  update: function(url, callbackSuccess, callbackFailure) {
+    var self = this;
+
+    $.ajax({
+      url : url,
+      success : function(data) {
+        callbackSuccess.call(self, data);
+      },
+      error : callbackFailure
+    });
+
+  },
+
+  updateDHCPSuccess: function(data) {
+      console.log(data);
+
+    var self = this,
+        openLocked,
+        processedData = self.processDHCPData(data),
+        output, data;
+
+    output = self.dhcpTemplate.render({
+      clientCount: processedData['countClients']
+    });
+
+    $('#msg').find('#clientcount').html(output);
+
+  },
+
+  updateDoorSuccess: function(data) {
+    var self = this,
+        processedData = self.processDoorData(data),
+        output;
+
+    if(processedData['doorStatus'] === 'unlocked'){
+        openLocked = 'padlock-open.png';
+    } else {
+        openLocked = 'padlock-closed.png';
+    }
+
+    output = self.doorTemplate.render({
+      openLocked: openLocked,
+      time: processedData['date']
+    });
+
+    $('#msg').find('#doorstatus').html(output);
+
+  },
+
+  updateFailure: function() {
+    $('#msg').html('<div class="error">Houston, we had a problem.</div>');
+  },
+
+  processDHCPData: function(data) {
+    var self = this,
+        result = [],
+        dataArray = data.split(/\n/),
+
+        tempDate = dataArray[0],
+        tempDateArray = tempDate.split('/'), //0, 1, 2
+        tempTimeArray = tempDate.split(':'), //1, 2, 3
+
+        DD = tempDateArray[0],
+        MM = self.createMonthFromString(tempDateArray[1]),
+        YYYY = tempDateArray[2].split(':')[0],
+        hh = tempTimeArray[1],
+        mm = tempTimeArray[2],
+        ss = tempTimeArray[3];
+
+    result['dateTime'] = YYYY + '-' + MM + '-' + DD + ' @ ' + hh + ':' + mm + ':' + ss;
+    result['countClients'] = dataArray[1];
+
+    return result;
+  },
+
+  processDoorData: function(data) {
+    var self = this,
+        result = [],
+        dataArray = data.split(/\n/);
+
+        result['doorStatus'] = dataArray[0];
+        result['date'] = self.processTimestamp(dataArray[2]);
+
+    return result;
+  },
+
+  processTimestamp: function(timestamp) {
+    var self = this,
+        dateObj = new Date(Number(timestamp*1000)),
+
+        YYYY = dateObj.getFullYear(),
+        MM = self.dateHelper(dateObj.getMonth() + 1),
+        DD = self.dateHelper(dateObj.getDate()),
+        hh = self.dateHelper(dateObj.getHours()),
+        mm = self.dateHelper(dateObj.getMinutes());
+
+    return YYYY + '-' + MM + '-' + DD + ' @ ' + hh + ':' + mm;
+
+  },
+
+  dateHelper: function(date) {
+      var result;
+
+      if(date < 10) {
+          date = '0'+date;
+      }
+
+      return date + ''
+  },
+
+  createMonthFromString: function(monthString) {
+    var self = this,
+        months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        month = months.indexOf(monthString) + 1;
+
+    month = self.dateHelper(month);
+
+    return month;
+
+  },
+
+  tick: function() {
+    var self = this;
+
+    self.update(self.dhcpUrl, self.updateDHCPSuccess, self.updateFailure);
+    self.update(self.doorUrl, self.updateDoorSuccess, self.updateFailure);
+
+  },
+
+  bindToView: function() {
+    var self = this;
+
+    $('#update').bind('click', function() {
+      self.tick();
+    });
+
+  }
+};
+
+
+$(document).ready(function() {
+
+  App.init();
+
+});
+
